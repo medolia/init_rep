d09d4fc188b3ec7570e1f86a19bb543e7df1da3c
medolia97 <458972801@qq.com>
2020-03-22 16:29:16 +0800

    optimize luckyGoogle

--- luckyGoogle.py
+++ luckyGoogle.py
@@ -6,21 +6,34 @@ import pyperclip
 import requests
 import bs4
 
-# define searching engine
-print('百度一下, 你就知道!')
-headers = {
-    'user-agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
-}
-url = 'https://www.baidu.com/s?wd=' + pyperclip.paste()
-res = requests.get(url, headers=headers)
 
-# Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text, "html.parser")
+def open_url():
+    print('百度一下, 你就知道!')
+    headers = {
+        'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+    }
+    if not pyperclip.paste():
+        print("Pyperclip is empty!")
+    else:
+        url = 'https://www.baidu.com/s?wd=' + pyperclip.paste()
+    res = requests.get(url, headers=headers)
 
-# Open a browser tab for each result.
-# 结果数可能小于 5
-tgt = soup.find_all(class_="t")
-linkNum = min(5, len(tgt))
-for i in range(linkNum):
-    webbrowser.open(tgt[i].find("a")["href"])
+    return res
+
+
+def show_result(res):
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    tgt = soup.find_all(class_="t")
+    linkNum = min(5, len(tgt))
+    for i in range(linkNum):
+        webbrowser.open(tgt[i].find("a")["href"])
+
+
+def main():
+    res = open_url()
+    show_result(res)
+
+
+if __name__ == "__main__":
+    main()
